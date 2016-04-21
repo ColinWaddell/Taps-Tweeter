3e93c5989d0bcba3265668983efe891e8698a21e
Colin Waddell <mrcolin@gmail.com>
2016-04-21 22:20:48 +0100

    Tidied up and commented code

--- tapstweeter.py
+++ tapstweeter.py
@@ -9,28 +9,37 @@ import csv
 CACHE_DIR = "sent_cache"
 SITE_URL = "http://taps-aff.co.uk"
 
-#Load API keys
+# Load API keys
 execfile('config.py')
 
 def generate_location_date_filename(location):
   return CACHE_DIR+"/"+location+"-"+time.strftime("%Y-%m-%d")+".csv"
 
+# Check if the weather is "aff" in given location
+# Returns the status from taps-aff if True.
 def get_taps_status(location):
   url = "http://taps-aff.co.uk/?api&location="+location
   response = urllib.urlopen(url)
   data = json.loads(response.read())
-  if data['taps']=='oan':
+  if data['taps']=='aff':
     return data
   else:
     return None
 
+# Write the taps-aff status to a csv file
 def stash_tapsaff_info(status, filename):
     w = csv.writer(open(filename, "w"))
     for key, val in status.items():
         w.writerow([key, val])
 
+# [Redundant Abstraction #1]
+def tweet_already_sent(filename):
+  return os.path.isfile(filename)
+
+# Attempt to send tweet and return True if
+# successful or False if there was a problem
 def send_tweet(tweet):
-  #cfg pulled in from config.py
+  # API_KEYS pulled in from config.py
   try:
     api = get_api(API_KEYS)
     print "Attempting to tweet("+str(len(tweet))+"): "+tweet
@@ -47,12 +56,19 @@ def get_api(cfg):
   return tweepy.API(auth)
 
 def main(location):
+  # Location to dump info if it's Taps Aff
   cache_file = generate_location_date_filename(location)
-  if os.path.isfile(cache_file):
+
+  if(tweet_already_sent(cache_file)):
     print "Already sent a tweet about "+location
     return
 
+  # Check if it's Taps Aff. If it is
+  # then send a tweet about it. If the
+  # tweet is successfull then save the
+  # info of the even in cache_file.
   taps_data=get_taps_status(location)
+
   if taps_data!=None:
     message="Officially #TapsAff in "+location.upper()+"! "+SITE_URL+" [I'm a robot]"
     success = send_tweet(message)
@@ -62,6 +78,7 @@ def main(location):
     print ("Taps Oan")
 
 if __name__ == "__main__":
+  # Must specify a single-word location
   if len(sys.argv)>1:
     location = sys.argv[1]
     main(location)
